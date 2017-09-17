021631da8328758c51550997ad00f743a5d524ee
Mohammad MDSA <samimd.77@gmail.com>
2017-09-17 13:43:54 +0430

    Add end dialog trigger action

--- app.js
+++ app.js
@@ -3,22 +3,22 @@ const builder = require('botbuilder');
 
 let server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, () => {
-	console.log('%s listening to %s', server.name, server.url); 
- });
- 
- // Create chat connector for communicating with the Bot Framework Service
- let connector = new builder.ChatConnector({
-	 appId: process.env.MICROSOFT_APP_ID,
-	 appPassword: process.env.MICROSOFT_APP_PASSWORD
- });
- 
- // Listen for messages from users 
- server.post('/api/messages', connector.listen());
- 
- // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
- let bot = new builder.UniversalBot(connector, (session) => {
-	 session.replaceDialog('pTypes');
- });
+	console.log('%s listening to %s', server.name, server.url);
+});
+
+// Create chat connector for communicating with the Bot Framework Service
+let connector = new builder.ChatConnector({
+	appId: process.env.MICROSOFT_APP_ID,
+	appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
+
+// Listen for messages from users 
+server.post('/api/messages', connector.listen());
+
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+let bot = new builder.UniversalBot(connector, (session) => {
+	session.replaceDialog('pTypes');
+});
 
 // Simple waterfall
 bot.dialog('greetings', [
@@ -72,65 +72,65 @@ bot.dialog('pTypes', [
 	// Choice prompt
 	(session, results) => {
 		session.send(`You chose ${results.response.entity}`);
-			builder.Prompts.choice(session, 'Which color do you prefer?', {
-				"west": {
-					units: 200,
-					total: "$6,000"
-				},
-				"central": {
-					units: 100,
-					total: "$3,000"
-				},
-				"east": {
-					units: 300,
-					total: "$9,000"
-				}
+		builder.Prompts.choice(session, 'Which color do you prefer?', {
+			"west": {
+				units: 200,
+				total: "$6,000"
+			},
+			"central": {
+				units: 100,
+				total: "$3,000"
+			},
+			"east": {
+				units: 300,
+				total: "$9,000"
 			}
+		}
 			, { listStyle: 3 }
 		);
-		
+
 	}
 ])
-.triggerAction({
-	confirmPrompt: "This will cancel your current request. Are you sure?"	
-});
+	.triggerAction({
+		confirmPrompt: "This will cancel your current request. Are you sure?"
+	});
 
 // The dialog stack is cleared and this dialog is invoked when the user enters 'help'.
 bot.dialog('help', function (session, args, next) {
-    session.endDialog("Global help dialog. <br/>Please say 'next' to continue");
+	session.endDialog("Global help dialog. <br/>Please say 'next' to continue");
 })
-.triggerAction({
-    matches: /^help1$/i,
-});
+	.triggerAction({
+		matches: /^help1$/i,
+	});
 
 // Global help dialog with action
 bot.dialog('helpWhitAction', function (session, args, next) {
-    session.endDialog("Global help dialog. <br/>Please say 'next' to continue");
+	session.endDialog("Global help dialog. <br/>Please say 'next' to continue");
 })
-.triggerAction({
-    matches: /^help$/i,
-    onSelectAction: (session, args, next) => {
-        // Add the help dialog to the dialog stack 
-        // (override the default behavior of replacing the stack)
-        session.beginDialog(args.action, args);
-    }
-});
+	.triggerAction({
+		matches: /^help$/i,
+		onSelectAction: (session, args, next) => {
+			// Add the help dialog to the dialog stack 
+			// (override the default behavior of replacing the stack)
+			session.beginDialog(args.action, args);
+		}
+	});
 
 // Dialog to ask for number of people in the party
 bot.dialog('askForPartySize', [
-    function (session) {
-        builder.Prompts.text(session, "How many people are in your party?");
-    },
-    function (session, results) {
-       session.endDialogWithResult(results);
-    }
+	function (session) {
+		builder.Prompts.text(session, "How many people are in your party?");
+	},
+	function (session, results) {
+		session.endDialogWithResult(results);
+	}
 ])
-.beginDialogAction('partySizeHelpAction', 'partySizeHelp', { matches: /^help$/i });
+	.beginDialogAction('partySizeHelpAction', 'partySizeHelp', { matches: /^help$/i });
 
 // Context Help dialog for party size
-bot.dialog('partySizeHelp', function(session, args, next) {
-    var msg = "Party size help: Our restaurant can support party sizes up to 150 members.";
-    session.endDialog(msg);
+bot.dialog('partySizeHelp', function (session, args, next) {
+	var msg = "Party size help: Our restaurant can support party sizes up to 150 members.";
+	session.endDialog(msg);
 });
 
 // Replacing dialogs with confirmation
@@ -157,38 +157,50 @@ bot.dialog('pTTypes', [
 	// Choice prompt
 	(session, results) => {
 		session.send(`You chose ${results.response.entity}`);
-			builder.Prompts.choice(session, 'Which color do you prefer?', {
-				"west": {
-					units: 200,
-					total: "$6,000"
-				},
-				"central": {
-					units: 100,
-					total: "$3,000"
-				},
-				"east": {
-					units: 300,
-					total: "$9,000"
-				}
+		builder.Prompts.choice(session, 'Which color do you prefer?', {
+			"west": {
+				units: 200,
+				total: "$6,000"
+			},
+			"central": {
+				units: 100,
+				total: "$3,000"
+			},
+			"east": {
+				units: 300,
+				total: "$9,000"
 			}
+		}
 			, { listStyle: 3 }
 		);
 	}
 ])
-.triggerAction({
-    matches: /^dinner reservation$/i,
-    confirmPrompt: "This will cancel your current request. Are you sure?"
-});
+	.triggerAction({
+		matches: /^dinner reservation$/i,
+		confirmPrompt: "This will cancel your current request. Are you sure?"
+	});
 
 // Ending Dialog with results(output)
 bot.dialog('orderDinner', [
-    //...waterfall steps...
-    // Last step
-    function(session, results){
-        if(results.response){
-            session.dialogData.room = results.response;
-            var msg = `Thank you. Your order will be delivered to room #${session.dialogData.room}`;
-            session.endConversation(msg);
-        }
-    }
+	//...waterfall steps...
+	// Last step
+	function (session, results) {
+		if (results.response) {
+			session.dialogData.room = results.response;
+			var msg = `Thank you. Your order will be delivered to room #${session.dialogData.room}`;
+			session.endConversation(msg);
+		}
+	}
 ]);
+
+// End dialog trigger action
+bot.dialog('dinnerOrder', [
+	//...waterfall steps...
+])
+	.endConversationAction(
+	"endOrderDinner", "Ok. Goodbye.",
+	{
+		matches: /^cancel$|^goodbye$/i,
+		confirmPrompt: "This will cancel your order. Are you sure?"
+	}
+	);
